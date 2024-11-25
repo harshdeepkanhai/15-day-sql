0b9db558330aeb9a335e46bfef73672276002255
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2024-11-25 23:59:40 +0530

    Add Order By SQL

---
+++ day_1/order_by.sql
@@ -0,0 +1,13 @@
+-- SELECT
+-- first_name,
+-- last_name,
+-- email
+-- FROM customer
+-- ORDER BY last_name DESC, first_name DESC;
+
+SELECT
+first_name,
+last_name,
+email
+FROM customer
+ORDER BY last_name DESC, first_name DESC;
