125221b55b6b56b207ac9bf77010f3be8c0584eb
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2024-12-16 15:16:13 +0530

    Add CASE with SUM and pivot

---
+++ day_5/CASE_SUM_with_pivot.sql
@@ -0,0 +1,22 @@
+SELECT
+SUM(CASE
+	WHEN rating = 'G' THEN 1
+	ELSE 0
+END) AS "G",
+SUM(CASE
+	WHEN rating = 'R' THEN 1
+	ELSE 0
+END) AS "R",
+SUM(CASE
+	WHEN rating = 'NC-17' THEN 1
+	ELSE 0
+END) AS "NC-17",
+SUM(CASE
+	WHEN rating = 'PG-13' THEN 1
+	ELSE 0
+END) AS "PG-13",
+SUM(CASE
+	WHEN rating = 'PG' THEN 1
+	ELSE 0
+END) AS "PG"
+FROM film;
